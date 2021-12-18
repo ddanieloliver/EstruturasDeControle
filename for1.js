6beb4c5352ac8e618c40f33165f44599f4145098
Daniel OLiveira <dnielvitoroliveira456@gmail.com>
2021-12-17 22:40:21 -0300

    Usando laço de repetição

--- for1.js
+++ for1.js
@@ -1,15 +1,15 @@
 let contador = 1
-while(contador <= 10) {
-   console.log (`contador = ${contador}`)
-   contador++
+while (contador <= 10) {
+    console.log(`contador = ${contador}`)
+    contador++
 }
 
-for( let i = 1; i <= 10; i++) {
+for (let i = 1; i <= 10; i++) {
     console.log(`i = ${i}`)
 }
 
 const notas = [6.7, 7.4, 9.8, 8.1, 7.7]
-for(let i=0;i<notas.length; i++){
+for (let i = 0; i < notas.length; i++) {
     console.log(`nota = ${notas[i]}`)
 } // length é o comprimento do array.
 //colocar let i para avariavel i ficar dentro do escopo e não ficar visivel fora.
